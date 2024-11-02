80aa9f0af91e25faed2b6e6955f00fae07684650
Roton02 <rph645102@gmail.com>
2024-11-02 15:39:02 +0600

    explain type and interface

--- Modiul2/2.2.ts
+++ Modiul2/2.2.ts
@@ -1,6 +1,6 @@
 {
   //Interface VS Type
-  // type primitive non primitive 2 jaigay use kora jai  but interface only non primitive
+  // type primitive non primitive 2 jaigay use kora jai  but interface only object (object , array , function ) kaj kore . 
   type user1 = {
     name: string;
     roll: number;
@@ -34,6 +34,25 @@
     roll: 4,
   role: "manager",
   };
+  // array 
+  type users1 = string[] 
+  const users : users1 = ['user1', 'user2', 'user3', 'user4']
+
+  interface users2 {
+    [index : number] : number
+   }
+const users6 : users2 = [123,23,43,1,2,12,3,23,34,1,21]
+
+   
+   //function 
+   type Add =(x:number , y: number)=> number 
+   const  add : Add = (x,y )=> x+y
+
+   interface Add2{
+    (x:number , y:number) : number
+   }
+   const  add2 : Add2 = (x,y )=> x+y
+
 
   //
 }
