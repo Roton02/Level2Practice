6ecd5aee9e1dd8226e43ee67e9ffea0f8ae87ac4
Roton02 <rph645102@gmail.com>
2024-11-02 13:10:47 +0600

    learn how extend new item in object using type and interface

--- Modiul2/2.2.ts
+++ Modiul2/2.2.ts
@@ -1,6 +1,6 @@
 {
   //Interface VS Type
-
+  // type primitive non primitive 2 jaigay use kora jai  but interface only non primitive
   type user1 = {
     name: string;
     roll: number;
@@ -17,6 +17,22 @@
   const user2: user2 = {
     name: "user2",
     roll: 2,
+  };
+
+  // practice using intersection that means & operator
+  type user3 = user1 & {
+    role : string ;
+  }
+  const user3: user3 = {
+    name: "user3",
+    roll: 3,
+    role: "admin",
+  };
+  interface user4 extends user2{ role : string}
+  const user4: user4 = {
+    name: "user4",
+    roll: 4,
+  role: "manager",
   };
 
   //
