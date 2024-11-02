faf1d6a9ae512213be0ee013fb72e17c3a2bf548
Roton02 <rph645102@gmail.com>
2024-11-02 21:21:23 +0600

    learn new that is  generic use interface and type

--- Modiul2/2.4.ts
+++ Modiul2/2.4.ts
@@ -1,5 +1,5 @@
 {
-  // Clean code using interface or type alias   
+  // Clean code using interface or type alias
   type GenariceArry<T> = Array<T>;
   interface MezbaVai {
     name: string;
@@ -18,7 +18,59 @@
       mobile: 1222939399,
     },
   ];
+
   // Generice with interface
+  interface Developer<T, x = null> {
+    name: string;
+    age: number;
+    bike: {
+      brand: string;
+      model: string;
+    };
+    smartWacth: T;
+    macbook?: x;
+  }
+  type apple = { model: string; color: string };
+  const poorDeveloper: Developer<apple> = {
+    name: "roton",
+    age: 21,
+    bike: {
+      brand: "honda",
+      model: "cbr1000rr",
+    },
+    smartWacth: {
+      model: "apple watch",
+      color: "black",
+    },
+  };
+
+  interface sumsung {
+    model: string;
+    price: number;
+    color: string;
+  }
+  interface mac {
+    model: string;
+    color: string;
+  }
+
+  const richDeveloper: Developer<sumsung, mac> = {
+    name: "sabbir",
+    age: 24,
+    bike: {
+      brand: "yamaha",
+      model: "yZF-R1",
+    },
+    smartWacth: {
+      model: "samsung",
+      price: 10203023,
+      color: "white",
+    },
+    macbook: {
+      model: "2021",
+      color: "gold",
+    },
+  };
 
   //
 }
