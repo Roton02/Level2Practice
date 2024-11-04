6fc7ace8ef50c95a488bdab21ce0080ac06a6536
Roton02 <rph645102@gmail.com>
2024-11-04 07:04:45 +0600

    practice task ongoing

--- Practice-Task/index.ts
+++ Practice-Task/index.ts
@@ -25,16 +25,55 @@
   }
   {
     //Task 3: Object Types, Type Alias, & Literal Types
-    type Person ={
-        name: string;
-        age: number;
-        gender: "male" | "female" ,
-        contactNo?: string;
-        address: string;
-        friends : string[]
+    type Person = {
+      name: string;
+      age: number;
+      gender: "male" | "female";
+      contactNo?: string;
+      address: string;
+      friends: string[];
+    };
+    const res: Person = {
+      name: "Rafi",
+      age: 23,
+      gender: "male",
+      contactNo: "01712345678",
+      address: "Dhaka",
+      friends: ["Rana", "Taimur", "Shahin"],
+    };
+  }
+  {
+    // Task 4: Union and Intersection Types
+    interface Book {
+      name: string;
+      author: string;
+      year: number;
     }
+    interface Magazine {
+      name: string;
+      publisher: string;
+      contract: number;
+    }
+    type BookUnionMagazine = Book | Magazine;
+    type BookIntersection = Book & Magazine
 
-
+    const res1 : BookUnionMagazine = {
+        name : 'Book Union',
+        author : 'Book',
+        year : 2022, 
+        contract : 10000
+    }
+    const res2 : BookIntersection ={
+        name : 'Book Intersection',
+        publisher : 'Book',
+        contract : 11,
+        year : 2020 ,
+        author : 'Book'
+    }
+  }
+  {
+    // Task 5: Function Type 
+    
   }
 
   //
